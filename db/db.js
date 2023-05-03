8b9a2367e4be30189ca98233da03f9d25850ef7e
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-05-03 15:00:25 -0300

    falso db

---
+++ db/db.js
@@ -0,0 +1,33 @@
+//false db: (json)
+
+const DB = {
+    games: [
+        {
+          id: 01,
+          title: 'Fortnite',
+          year: 2011
+        },
+        {
+            id: 02,
+            title: 'Minecraft',
+            year: 2011
+        },
+        {
+            id: 03,
+            title: 'Animal Crossing: New Horizons',
+            year: 2020
+        },
+        {
+            id: 04,
+            title: 'Assassins Creed Valhalla',
+            year: 2020
+        },
+        {
+            id: 05,
+            title: 'Cyberpunk 2077',
+            year: 2020
+        },
+    ]
+}
+
+module.exports = DB;

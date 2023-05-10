5d8c43fe93245b2cbb28e7bb31994e272d144792
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-05-10 14:23:08 -0300

    utilizando o cors para consumir a api

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const db = require('./db/db');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 

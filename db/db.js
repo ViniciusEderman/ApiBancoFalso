6ceb662591f0d4545e533f9af4e6993b682221f7
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-05-15 17:42:06 -0300

    criando dados falsos para login de user

--- db/db.js
+++ db/db.js
@@ -27,6 +27,20 @@ const DB = {
             title: 'Cyberpunk 2077',
             year: 2020
         },
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Victor Luis",
+            email: "victor.luis@gmail.com",
+            password: "victorluis2030"
+        },
+        {
+            id: 2,
+            name: "Luis Carlos",
+            email: "carlinhos@gmail.com",
+            password: "20301015"
+        },
     ]
 }
 
